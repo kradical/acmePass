0fd64295b60f3c9f4c0170d7008519a71d3ac8e4
Konrad Schultz <konrad_here@hotmail.com>
2017-05-30 13:07:30 -0700

    multiple tests in the same browser

--- src/test/javascript/webdriver/test.js
+++ src/test/javascript/webdriver/test.js
@@ -1,20 +1,36 @@
-var webdriverio = require('webdriverio');
-var options = {
+const webdriverio = require('webdriverio');
+
+const options = {
     desiredCapabilities: {
         browserName: 'chrome'
     }
-};
+}
+
+let client;
 
 describe('The initial test', function() {
+    before(function(){
+        client = webdriverio.remote(options);
+        return client.init();
+    });
+
     it('should open the browser', function() {
-        return webdriverio
-            .remote(options)
-            .init()
+        return client
             .url('http://www.google.com')
-            .waitForVisible('#hplogo')
             .getTitle().then(function(title) {
                 console.log('Title was: ' + title);
-            })
-            .end();
-    })
-})
+            });
+    });
+
+    it('should open the browser', function() {
+        return client
+            .url('http://www.facebook.com')
+            .getTitle().then(function(title) {
+                console.log('Title was: ' + title);
+            });
+    });
+
+    after(function() {
+        return client.end();
+    });
+});
